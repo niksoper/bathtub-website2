7081ed895f1aa3137f9784c567a13966f16ff1bc
Nicholas Soper <nick@rocketmakers.com>
2021-01-01 14:10:26 +0000

    Fix casing in shell import for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import Shell from '../Layout/shell';
+import Shell from '../layout/shell';
 
 const NotFoundPage = () => (
   <Shell>
--- types/graphql-types.ts
+++ types/graphql-types.ts
@@ -1201,8 +1201,6 @@ export type QueryAllDirectoryArgs = {
 export type QuerySiteArgs = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
-  port?: Maybe<IntQueryOperatorInput>;
-  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
@@ -1317,8 +1315,6 @@ export type QueryAllSitePluginArgs = {
 export type Site = Node & {
   buildTime?: Maybe<Scalars['Date']>;
   siteMetadata?: Maybe<SiteSiteMetadata>;
-  port?: Maybe<Scalars['Int']>;
-  host?: Maybe<Scalars['String']>;
   polyfill?: Maybe<Scalars['Boolean']>;
   pathPrefix?: Maybe<Scalars['String']>;
   id: Scalars['ID'];
@@ -1523,8 +1519,6 @@ export type SiteFieldsEnum =
   | 'siteMetadata___siteName'
   | 'siteMetadata___siteUrl'
   | 'siteMetadata___companyName'
-  | 'port'
-  | 'host'
   | 'polyfill'
   | 'pathPrefix'
   | 'id'
@@ -1617,8 +1611,6 @@ export type SiteFieldsEnum =
 export type SiteFilterInput = {
   buildTime?: Maybe<DateQueryOperatorInput>;
   siteMetadata?: Maybe<SiteSiteMetadataFilterInput>;
-  port?: Maybe<IntQueryOperatorInput>;
-  host?: Maybe<StringQueryOperatorInput>;
   polyfill?: Maybe<BooleanQueryOperatorInput>;
   pathPrefix?: Maybe<StringQueryOperatorInput>;
   id?: Maybe<StringQueryOperatorInput>;
